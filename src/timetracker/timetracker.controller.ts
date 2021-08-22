659d1cd86efc88cbc594f685cab880bcaf574b26
Gabriel Falcones <gafalcon@espol.edu.ec>
2021-08-21 20:45:27 -0500

    Validate start, stop timeslots

--- src/timetracker/timetracker.controller.ts
+++ src/timetracker/timetracker.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { ConflictException, Controller, Get, Param, Post } from '@nestjs/common';
 import { TimeTrackService } from './timetrack.service';
 
 @Controller('projects')
@@ -18,13 +18,17 @@ export class TimeTrackerController {
 
     @Post(':project/start')
     async startTime(@Param('project') project: string){
+        const timeslot = await this.timetrackService.findRunningTimeSlot(project)
+        if (timeslot)
+            throw new ConflictException('Project already running')
         return this.timetrackService.startTimeSlot(project)
     }
 
     @Post(':project/stop')
     async stopTime(@Param('project') project: string){
         const timeslot = await this.timetrackService.findRunningTimeSlot(project)
+        if (!timeslot)
+            throw new ConflictException('Project not found or not running')
         return this.timetrackService.stopTimeSlot(timeslot)
     }
-
 }
